8e4b52f1b8fd9f972a972372782d1b8677082ca0
Ian Jackson <iansjackson8@gmail.com>
2024-03-15 14:16:39 -0400

    add homepage components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomepageComponent,
+    title: "中文字典｜家"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
